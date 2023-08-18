de8436f568a9d9ce6b941637e53595de25ff1455
“KseGreb” <ksegreb@gmail.com>
2023-08-18 13:26:57 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,78 @@
 import { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
+import video from "./1.mp4"
 import Nutrition from './Nutrition';
+import apple from "./apple.png"
+
 
 function App() {
+
+
 
   const MY_ID = "7f0c4b91";
   const MY_KEY = "60c83dfc015d4c049cd203bebc2506b6";
 
-  const [myIngredient, setMyIngredient] = useState("");
-  const [showNutrition, setShowNutrition] = useState([]);
+  const [myIngredientSearch, setMyIngredientSearch] = useState("");
+  const [showCalories, setShowCalories] = useState("");
+  const [showFat, setShowFat] = useState("");
+  const [showCholesterol, setShowCholesterol] = useState("");
 
   useEffect(()=>{
     const getNutritientsInfo = async () =>{
     const response = await fetch (`https://api.edamam.com/api/nutrition-data?app_id=${MY_ID}&app_key=${MY_KEY}&nutrition-type=logging&ingr=salmon`)
       const data = await response.json();
-      console.log(data.totalWeight
-        );
-      setShowNutrition(data);
+      console.log(data);
+      setShowCalories(data.calories);
+      setShowFat(data.totalNutrients.FAT.quantity);
+      setShowCholesterol(data.totalNutrients.CHOLE.quantity)
+
     }
     getNutritientsInfo()
   }, [])
 
   const inputIngredient = (e) => {
     console.log(e.target.value)
-    setMyIngredient(e.target.value)
+    setMyIngredientSearch(e.target.value)
   }
+
+
 
   return (
     <div className="App">
+      <div className='container'>
+        <h1>Nutrition Analysis API</h1>
+      </div>
 
-      <h1>Nutrition Analysis API</h1>
+    <div  className='container'>
+      <p>Enter an ingredient list list for what you are cooking, like "1 cup rice, 10 oz chickpeas", etc.
+        Enter each ingredient on a new line.</p>
+    </div>
 
-    <form>
-      <input placeholder='Type your ingredient...' onChange={inputIngredient} value={myIngredient}/>
+        <video autoPlay muted loop>
+          <source src={video} type = "video/mp4"/>
+        </video>
+
+      
+
+    <form className='container'>
+      <input placeholder='Type your ingredient...' onChange={inputIngredient} value={myIngredientSearch}/>
     </form>
+    <div  className='container'>
+      <button>
+        <img src={apple} alt='icon' width="25px"/> Search
+        <img src={apple} alt='icon' width="25px"/>
+      </button>
+    </div>
 
-{showNutrition.map((element) =>(
-  <Nutrition/>
-))}
+    
+
+    
+    <Nutrition data={showCalories}/>
+
+    <p>Calories: {showCalories} </p>
+    <p>Total fat: {showFat} g</p>
+    <p>Cholesterol: {showCholesterol} mg</p>
 
     </div>
   );
--- src/Nutrition.js
+++ src/Nutrition.js
@@ -1,7 +1,16 @@
-function Nutrition(){
+function Nutrition({data, showCalories}){
     return(
-        <div>
-            <h2>test</h2>
+        <div className="container">
+            <div className="label">
+                <h2>Nutrition Facts</h2>
+                <p>Amount Per Serving</p>
+                <h2>Calories {showCalories}</h2>
+                
+                <h3>Total Fat {} g</h3>
+                <h3>Cholesterol {} mg</h3>
+                <p>*Percent Daily Values are based on a 2000 calorie diet</p>
+            </div>
+            
         </div>
     )
 }
